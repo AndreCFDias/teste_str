c9cb9d81da5d0725fc5c3f78a6b5627b36f3f6c9
AndreCFDias <100570083+AndreCFDias@users.noreply.github.com>
2023-09-12 12:00:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-
+from scipy.stats import norm
 
 opcao = st.sidebar.selectbox("Distributions", ("Normal", "Exponential",
                                               "Chi-squared", "T student", "Binomial", "Poisson"))
@@ -16,10 +16,12 @@ if opcao == "Normal":
     mu = st.sidebar.number_input('Mean:', step=0.5)
     sd = st.sidebar.number_input('Standard Deviation:', min_value=0.0, value=1.0, step=0.5)
     nor1 = np.random.normal(mu, sd, size=1000)
-    
+    nor = np.linspace(norm.ppf(0.00000000001, mu, sd), norm.ppf(0.99999999999, mu, sd), 100)
 
     fig, ax = plt.subplots()
     ax.set_ylabel("Probability Density")
+    if curve:
+        ax.plot(nor, norm.pdf(nor, mu, sd), 'r-', lw=3, alpha=0.7, label='Normal')
     if histog:
         ax.hist(nor1, 40, range=(-8, 8), density=True, alpha=0.6)
     plt.xlim(-9, 9)
